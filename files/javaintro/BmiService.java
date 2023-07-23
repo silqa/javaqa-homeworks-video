808451511a278ce1445a650f569f2ff63cff688d
Anastasia Silqa Lapteva <stasya.mango@gmail.com>
2023-07-23 15:43:34 +0300

    4. Testability, авто-тесты, введение в ООП: объекты и методы Задание 2. Индекс массы тела (необязательная задача)

---
+++ files/javaintro/BmiService.java
@@ -0,0 +1,5 @@
+public class BmiService {
+    public float calculate (float weight, float height) {
+        return weight / (height*height);
+    }
+}
--- files/javaintro/Main.java
+++ files/javaintro/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        double miles = service.calculate(price);
-        System.out.println(miles);
+        BmiService service = new BmiService();
+        float weight = 85;
+        float height = 1.75f;
+        double bmi = service.calculate(weight, height);
+        System.out.println(bmi);
     }
 }
